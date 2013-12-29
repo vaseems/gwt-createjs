58f4c40f3cc1c13e9c1ed3a6c45ccaa94383a160
Vaseem <diveincode@gmail.com>
2013-12-29 19:11:02 +0530

    PreloadJS project created

---
+++ gwt-createjs/src/com/voisintech/client/GwtCreateJS.java
@@ -0,0 +1,12 @@
+package com.voisintech.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GwtCreateJS implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+
+	}
+
+}
